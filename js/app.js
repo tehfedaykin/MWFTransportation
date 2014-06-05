719bbbdb9b122683e16fa111a92f2a09584bf326
Jennifer <jwadella@vml.com>
2014-06-05 16:22:26 -0500

    adding filtering. w00t

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ $(document).ready(function(){
     var block = $(this).attr('href');
     $('.jumbotron').hide();
     $(block).show();
+    console.log('clicked');
+  });
 
+  $('#searchName').click(function(){
+    var name = $('#name').val();
+    var row = $('table').find('tr');
+    for (var i = 0; i < row.length; i++) {
+      var $row = $(row[i]);
+      var string = $row.html().toString();
+      if(string.indexOf(name) <0){
+        $row.hide();
+      }
+      else{
+        $row.show();
+      }
+    };
   });
 })
